b3ad4ae1cb2d36b9869571bd5d23ee59f274f3e9
Constantine Shibaev <killtobanana@gmail.com>
2019-04-04 08:49:01 +0300

    added some secondary components

--- src/app/_components/weather/weather.component.ts
+++ src/app/_components/weather/weather.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent implements OnInit {
+  public weatherTitle = 'The Weather in this hotel';
 
   constructor() { }
 
